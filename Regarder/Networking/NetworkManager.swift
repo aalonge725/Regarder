e41c5da119e4df8442ab69461db57332a09d9176
Abraham Alonge <aalonge@alumni.unc.edu>
2024-06-14 22:07:50 -0400

    Add network manager

---
+++ Regarder/Networking/NetworkManager.swift
@@ -0,0 +1,20 @@
+//
+//  NetworkManager.swift
+//  Regarder
+//
+//  Created by Abraham Alonge on 6/14/24.
+//
+
+import Foundation
+
+struct NetworkManagerer {
+    static func get<T: Decodable>(url: URL, responseType: T.Type, urlSession: URLSession = .shared) async throws -> T {
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+
+        let (data, _) = try await urlSession.data(for: request)
+        let response = try JSONDecoder().decode(responseType, from: data)
+        
+        return response
+    }
+}
