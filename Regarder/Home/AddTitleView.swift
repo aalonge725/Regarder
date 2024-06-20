e5aafeeec30778208a1a1d17bf4ff60e9b369bf7
Abraham Alonge <aalonge@alumni.unc.edu>
2024-06-20 17:09:09 -0400

    Create sheet for Add Title view

---
+++ Regarder/Home/AddTitleView.swift
@@ -0,0 +1,18 @@
+//
+//  AddTitleView.swift
+//  Regarder
+//
+//  Created by Abraham Alonge on 6/20/24.
+//
+
+import SwiftUI
+
+struct AddTitleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTitleView()
+}
