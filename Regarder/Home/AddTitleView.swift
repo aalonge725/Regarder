2158cca937112ded4524c793818fb5f1957b0f41
Abraham Alonge <aalonge@alumni.unc.edu>
2024-06-21 01:32:59 -0400

    Add search text debouncing, OMDb API call, and alerts for errors

--- Regarder/Home/AddTitleView.swift
+++ Regarder/Home/AddTitleView.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 struct AddTitleView: View {
+    @StateObject var vm = AddTitleViewModel()
     @Binding var isAddTitleSheetShowing: Bool
-    @State private var searchResultTitles: [OMDbAPITitle] = []
-    @State private var searchText: String = ""
+    @State private var isInvalidSearchAlertShowing = false
+    @State private var isSearchRequestErrorAlertShowing = false
     
     var body: some View {
         NavigationStack {
@@ -20,7 +21,7 @@ struct AddTitleView: View {
                 
                 ScrollView {
                     VStack {
-                        if searchText.isEmpty { // TODO: fix weird animation when searchText goes from non-empty to empty
+                        if vm.searchText.isEmpty { // TODO: fix weird animation when searchText goes from non-empty to empty
                             Image(systemName: "magnifyingglass")
                                 .imageScale(.large)
                                 .font(.system(size: 50))
@@ -49,12 +50,41 @@ struct AddTitleView: View {
                     }
                 }
             }
-            .searchable(text: $searchText, prompt: "Search for a movie or tv show")
+            .searchable(text: $vm.searchText, prompt: "Search for a movie or tv show")
+            .onChange(of: vm.debouncedText) { oldText, newText in
+                if newText.isEmpty {
+                    return
+                }
+                
+                // TODO: check if newText is alphanumeric
+                
+                guard let url = URL(string: "https://www.omdbapi.com/?apikey=4ac97843&s=\(newText)") else {
+                    isInvalidSearchAlertShowing = true
+                    return
+                }
+                
+                Task {
+                    do {
+                        let response = try await NetworkManager.get(url: url, responseType: OMDbAPISearchResult.self) // TODO: make model for failed search result (ex. too many results or movie not found)
+                        vm.searchResultTitles = response.titles
+                    } catch {
+                        vm.searchRequestErrorDescription = error.localizedDescription
+                        isSearchRequestErrorAlertShowing = true
+                    }
+                }
+                
+            }
+            .alert("Invalid Search Query", isPresented: $isInvalidSearchAlertShowing) {} message: {
+                Text("Please only enter letters and/or numbers into the search bar")
+            }
+            .alert("Search Request Error", isPresented: $isSearchRequestErrorAlertShowing) {} message: {
+                Text("\(vm.searchRequestErrorDescription)")
+            }
         }
     }
     
     private var searchResultTitlesList: some View {
-        ForEach(searchResultTitles) { title in
+        ForEach(vm.searchResultTitles) { title in
             // TODO: make list
         }
     }
